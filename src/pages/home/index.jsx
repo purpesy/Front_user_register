06e0c4189633cad3af1e3139fb43042be041e275
Lucas <lucasgabrieldossantos753@gmail.com>
2025-01-09 11:25:59 -0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,10 +3,8 @@ import "./style.css";
 import Trash from "../../assets/lixo-icon.png";
 import api from "../../services/api";
 
-// useEffect e useState e useRef , são hooks do react(ferramentas) que auxiliam no desenvolvimento
-
 function Home() {
-  const [users, setUsers] = useState([]); // adiciono users, e o que vai entrar atraves do usestate que começa vazio
+  const [users, setUsers] = useState([]);
 
   const inputName = useRef();
   const inputAge = useRef();
@@ -38,7 +36,7 @@ function Home() {
   }, []);
 
   return (
-    // tudo que tiver fora do return é codigo do js, e dentro é codigo do html, exceto se eu colocar dentro do {}, podendo ser javascript
+    
     <div className="container">
       <form action="">
         <h1>Cadastrar-se</h1>
@@ -65,10 +63,6 @@ function Home() {
           </button>
         </div>
       ))}{" "}
-      {/* Nesta linha, esta pegando todos os arrays(usuarios) do users. e mapeando, ou seja, pegando todos, e transformando em user(um usuario por vez) 
-      Quando adiciono uma função com (), dentro dela coloco o html, e dentro do html, eu coloco {} que o que tiver dentro da chave, vai ser o que sera mostrado de acordo com a função users 
-      sempre que usar MAP = necessito adicionar uma key para conseguir usar.
-      */}
     </div>
   );
 }
